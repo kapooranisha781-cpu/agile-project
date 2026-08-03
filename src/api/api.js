fd9f622b2b4d79aed8fd58eb6e773f5a69f704c1
anishakapoor25-gif <anishakapoor25@navgurukul.org>
2026-08-03 11:57:25 +0530

    connect api with render backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://agile-project-1-urqo.onrender.com",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
